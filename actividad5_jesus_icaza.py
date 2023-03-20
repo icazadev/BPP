3c549fd910e7f2b0b01e90e41e0a0c422380479c
icazadev <icaza.dev@gmail.com>
2023-03-21 00:03:53 +0100

    Ejecucion y comprobacion de nuestras funciones definidas para verificar su correcta ejecucion

--- actividad5_jesus_icaza.py
+++ actividad5_jesus_icaza.py
@@ -7,6 +7,7 @@ pdb.set_trace()
 listas = [[2, 4, 1], [1,2,3,4,5,6,7,8], [100,250,43]]
 listas_numeros = [3, 4, 8, 5, 5, 22, 13]
 
+
 def getMaxNumbers(numLists: list) -> list:
     result = [max(listNums) for listNums in numLists]
     return result
@@ -17,3 +18,11 @@ def getPrimeNumbers(num:int) -> bool:
         return True
     return False
 
+
+# get max number for each list
+maxnumoflists = getMaxNumbers(listas)
+print(maxnumoflists)
+
+# get prime numbers
+prime_numbers = filter(getPrimeNumbers, listas_numeros)
+print(list(prime_numbers))
