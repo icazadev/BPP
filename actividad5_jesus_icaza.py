d71363d2123207af46849e90a5df5699acb622b4
icazadev <icaza.dev@gmail.com>
2023-03-20 23:59:50 +0100

    Definida la funcion que calcula el numero mayor de cada lista contenida en una lista superior

--- actividad5_jesus_icaza.py
+++ actividad5_jesus_icaza.py
@@ -6,3 +6,7 @@ pdb.set_trace()
 # list of num lists
 listas = [[2, 4, 1], [1,2,3,4,5,6,7,8], [100,250,43]]
 listas_numeros = [3, 4, 8, 5, 5, 22, 13]
+
+def getMaxNumbers(numLists: list) -> list:
+    result = [max(listNums) for listNums in numLists]
+    return result
